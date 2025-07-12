3c44be7b2026139863a7f6916b77ad1e595c5170
VamsiKrishna-D <vamsikrishna226645@gmail.com>
2025-07-12 15:51:09 +0530

    Add files via upload

---
+++ Day09/project61/app1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def  home(request):
+    temp=loader.get_template('home.html')
+    return HttpResponse(temp.render())
+
+
+# Create your views here.
+def  vamsi(request):
+    temp=loader.get_template('vamsi.html')
+    return HttpResponse(temp.render())
